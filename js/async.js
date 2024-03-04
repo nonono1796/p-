c5ab23699ad9dcf126e409fd342d849f3652459b
nozawamitsuki <kaminagara13@gmail.com>
2024-03-04 23:08:07 +0900

    非同期処理の学習

--- js/async.js
+++ js/async.js
@@ -10,3 +10,13 @@
      // 同期処理を実行する
      console.log('処理5');
      console.log('処理6');
+
+     // HTML要素をclassで取得し、定数に代入する。
+const btnElement=document.getElementById('btn');
+const textElement=document.getElementById('text');
+// ボタンをクリックした時に表示を変える
+btnElement.addEventListener('click',()=>{
+    setTimeout(() => {
+        textElement.textContent='ボタンをクリックしました';
+    }, 2000);
+})
