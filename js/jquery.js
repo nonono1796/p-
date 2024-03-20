3dc00c5529b9c32828ee01750a49f1422499e7a3
nozawamitsuki <kaminagara13@gmail.com>
2024-03-20 14:33:48 +0900

    属性フィルタ

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,12 @@
+// $(function(){
+//     const title = $('#title').text();
+//     const list = $('.message').text();
+//     console.log(title);
+//     console.log(list);
+// });
+
 $(function(){
-    const title = $('#title').text();
-    const list = $('.message').text();
-    console.log(title);
-    console.log(list);
+    const text = $('li[class*="list"]').text();
+    console.log(text);
 });
+
